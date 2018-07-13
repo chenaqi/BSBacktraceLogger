19c12ad266aee55ff909982d2cde235fe1ca124a
chenwuqi@hdfax.com <>
2018-07-13 11:26:50 +0800

    first commit

---
+++ BSBacktraceLogger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSBacktraceLogger
+//
+//  Created by hengda on 2018/7/13.
+//  Copyright © 2018年 Chen Nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BSBacktraceLogger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BSBacktraceLogger
+//
+//  Created by hengda on 2018/7/13.
+//  Copyright © 2018年 Chen Nate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
